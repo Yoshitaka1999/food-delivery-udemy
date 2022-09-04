b952a90d67c42f3c264e7961997b263633e83baf
加藤義隆 <991024yoshi@gmail.com>
2022-09-04 22:12:33 +0900

    フロントのセットアップ

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,14 @@
+import { Alert, Button } from "reactstrap";
+
 const index = () => {
-    return (
-        <div>
-            HELLO
-        </div>
-    );
-}
+  return (
+    <div>
+      <div>
+        <Alert color="primary">Hello Project</Alert>
+        <Button color="primary">Hello from Next.js</Button>
+      </div>
+    </div>
+  );
+};
 
 export default index;
