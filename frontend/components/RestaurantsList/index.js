a19136c5c8ff409b2273a99602bcc3f9ce14443c
加藤義隆 <991024yoshi@gmail.com>
2022-09-07 18:32:00 +0900

    APIデータで書き換え

--- frontend/components/RestaurantsList/index.js
+++ frontend/components/RestaurantsList/index.js
@@ -1,25 +1,69 @@
 import { Col, Row, Card, CardImg, CardBody, CardTitle } from "reactstrap";
 import Link from "next/link";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const query = gql`
+  {
+    restaurants {
+      id
+      name
+      description
+      image {
+        url
+      }
+    }
+  }
+`;
 
 const RestaurantList = () => {
-  return (
-    <Row>
-      <Col>
-        <Card>
-          <CardImg src="" />
-          <CardBody>
-            <CardTitle>Italian Restaurant</CardTitle>
-            <CardTitle>イタリアンのレストランです。</CardTitle>
-          </CardBody>
-          <div className="card-footer">
-            <Link href="/restaurants?id=1" as="/restaurants/1">
-                <a className="btn btn-primary">もっと見る</a>
-            </Link>
-          </div>
-        </Card>
-      </Col>
-    </Row>
-  );
+  const { loading, error, data } = useQuery(query);
+
+  if (data) {
+    return (
+      <Row>
+        {data.restaurants.map((res) => (
+          <Col xs="6" sm="4" key={res.id}>
+            <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+              <CardImg
+                src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
+                top={true}
+                style={{ height: 250 }}
+              />
+              <CardBody>
+                <CardTitle>{res.name}</CardTitle>
+                <CardTitle>{res.description}</CardTitle>
+              </CardBody>
+              <div className="card-footer">
+                <Link href={`/restaurants/${res.id}`} as={`/restaurants?id=${res.id}`}>
+                  <a className="btn btn-primary">もっと見る</a>
+                </Link>
+              </div>
+            </Card>
+          </Col>
+        ))}
+        <style jsx>
+          {`
+            a {
+              color: white;
+            }
+            a:link {
+              text-decoration: none;
+              color: white;
+            }
+            a:hover {
+              color: white;
+            }
+            .card-columns {
+              column-count: 3;
+            }
+          `}
+        </style>
+      </Row>
+    );
+  } else {
+    return <h1>レストランが見つかりませんでした。</h1>;
+  }
 };
 
 export default RestaurantList;
