004ff870a5ee5638c1860108eb92eaeac7efa40b
加藤義隆 <991024yoshi@gmail.com>
2022-09-11 00:18:32 +0900

    レストラン固有ページ作成

--- frontend/components/RestaurantsList/index.js
+++ frontend/components/RestaurantsList/index.js
@@ -42,7 +42,7 @@ const RestaurantList = (props) => {
                 <CardTitle>{res.description}</CardTitle>
               </CardBody>
               <div className="card-footer">
-                <Link href={`/restaurants/${res.id}`} as={`/restaurants?id=${res.id}`}>
+                <Link as={`/restaurants/${res.id}`} href={`/restaurants?id=${res.id}`}>
                   <a className="btn btn-primary">もっと見る</a>
                 </Link>
               </div>
